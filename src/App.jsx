b6e689665ba7f39f0cbd7f1211dae021c877335d
krishnarajan <krishh.v777@gmail.com>
2025-05-26 23:53:17 +0530

    rename lib and downgrade some dependancies

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./components/ui/toaster";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
+const queryClient = new QueryClient();
+
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Index />} />
-    <Route path="*" element={<NotFound />} />
-  </Routes>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
 
 export default App;
