f8a2da5aef1aba43d71d41cbab06afb2fc13a9c4
Krish V <krishh.v777@gmail.com>
2026-01-11 22:33:52 +0530

    year change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="flex flex-col h-[180px] md:h-[450px] w-full items-center justify-end pb-4 px-4">
         <TextHoverEffect text="KRISH" className="z-50" />
         <p className="text-white/80 text-xs md:text-sm text-center">
-          &copy; 2025 Krishnarajan. All rights reserved 🤍
+          &copy; 2026 Krishnarajan. All rights reserved 🤍
         </p>
       </div>
     </footer>
